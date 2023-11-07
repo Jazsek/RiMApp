fd750a694a4a8ad5550bb99df59baf4c127539d0
Jazsek <jaszek94@gmail.com>
2023-11-07 12:22:14 +0100

    Setup fillable, casts, and relations.

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Enum\CharacterGenderEnum;
+use App\Enum\CharacterStatusEnum;
+use App\Models\Episode;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Character extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'status',
+        'species',
+        'type',
+        'gender',
+        'image',
+    ];
+
+    protected $casts = [
+        'status' => CharacterStatusEnum::class,
+        'gender' => CharacterGenderEnum::class
+    ];
+
+    public function episodes(): BelongsToMany
+    {
+        return $this->belongsToMany(Episode::class);
+    }
 }
--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Character;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Episode extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'air_date',
+        'episode',
+    ];
+
+    public function characters(): BelongsToMany
+    {
+        return $this->belongsToMany(Character::class);
+    }
 }
